30464910e2a0870502bbaac8a9a246b88843f740
sharma <sharmasbm1296@gmail.com>
2021-03-13 23:16:14 -0800

    reset again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,79 +1,10 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import {BrowserRouter as Router,Route } from "react-router-dom"
-import Footer from './Components/Footer'
-import Header from './Components/Header'
-import cartScreen from './Screens/cartScreen'
-import HomeScreen from './Screens/HomeScreen'
-import LoginScreen from './Screens/LoginScreen'
-import OrderListScreen from './Screens/OrderListScreen'
-import OrderScreen from './Screens/orderScreen'
-import PaymentScreen from './Screens/PaymentScreen'
-import PlaceOrderScreen from './Screens/PlaceOrderScreen'
-import ProductEditScreen from './Screens/ProductEditScreen'
-import ProductListScreen from './Screens/ProductListScreen'
-import ProductScreen from './Screens/ProductScreen'
-import ProfileScreen from './Screens/ProfileScreen'
-import RegisterScreen from './Screens/RegisterScreen'
-import SearchScreen from "./Screens/SearchScreen"
-import ShippingScreen from './Screens/ShippingScreen'
-import UserEditScreen from './Screens/UserEditScreen'
-import userListScreen from './Screens/userListScreen'
+
 
 function App() {
   return (
   <>
-  <Router>
-  <Header/>
-  <main className='py-3'>
-    <Container>
-    <Route path='/order/:id' component={OrderScreen} />
-    <Route path='/shipping' component={ShippingScreen} />
-    <Route path='/payment' component={PaymentScreen} />
-    <Route path='/placeorder' component={PlaceOrderScreen} />   
-  <Route path='/login' component={LoginScreen} />
-  <Route path='/register' component={RegisterScreen} />
-  
-  <Route path='/profile' component={ProfileScreen} />
-    <Route path='/product/:id' component={ProductScreen} />
-    <Route path='/cart/:id?' component={cartScreen} /> 
-    <Route path='/admin/userlist' component={userListScreen} />
-    <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-    <Route
-            path='/admin/productlist'
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={ProductListScreen}
-            exact
-          />
-    <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
-    <Route path='/admin/orderlist' component={OrderListScreen} />
-    <Route path='/search/:keyword' component={HomeScreen} exact />
-   
-          <Route path='/page/:pageNumber' component={HomeScreen} exact />
-          <Route
-            path='/search/:keyword/page/:pageNumber'
-            component={HomeScreen}
-            exact
-          />
-           <Route
-            path="/search/category/:category"
-            component={SearchScreen}
-            exact
-          ></Route>
-          <Route
-            path="/search/category/:category/name/:name"
-            component={SearchScreen}
-            exact
-          ></Route>
-    <Route path='/' component={HomeScreen} exact />
-    </Container>
-  </main>
-  <Footer/>
-  </Router>
+ hlo
   </>
 
   )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./bootstrap.min.css";
 import './index.css';
-import { Provider } from 'react-redux'
-import store from './store'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
+
     <App />
-  </Provider>,
-  document.getElementById('root')
+
+ , document.getElementById('root')
 );
 
 
